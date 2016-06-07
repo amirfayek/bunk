304a674e384559743dd91121cd6cec6fee5001b9
Amir Fayek <afayek88@gmail.com>
2016-06-07 16:13:53 -0700

    refactor controller and views

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,50 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser')
 
 var Zillow = require('node-zillow');
 var zillow = new Zillow(process.env.ZILLOW_ID);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var parameters = {
-      state: req.param('state')
-    };
+    res.render('index');
+});
 
-    zillow.get('GetRegionChildren', parameters)
+/* POST home page. */
+router.post('/', function(req, res, next) {
+    var parameters = prepareZillowParams(req);
+    zillow.get('GetDeepSearchResults', parameters)
       .then(function(results) {
-        console.log(results.response.list)
-        res.render('index', results);
-        return results
+        var home = filterHomeResults(results);
+        res.send(JSON.stringify({home: home}));
       })
 });
+
+
+
+function prepareZillowParams(req) {
+    var cityStateZipEncoded = encodeURIComponent(req.body.city + req.body.state + req.body.zipcode);
+    var spacePattern = /\s/g;
+    if (req.body.address) var addressEncoded = req.body.address.replace(spacePattern, '+');
+    var parameters = {
+      address: addressEncoded,
+      citystatezip: cityStateZipEncoded,
+    };
+    return parameters
+}
+
+function filterHomeResults(results) {
+    var home = {
+        zpid: results.response.results.result[0].zpid,
+        address: results.response.results.result[0].address[0].street,
+        city: results.response.results.result[0].address[0].city,
+        state: results.response.results.result[0].address[0].state,
+        beedrooms: results.response.results.result[0].address[0].beedrooms,
+        bathrooms: results.response.results.result[0].address[0].bathrooms,
+        yearBuilt: results.response.results.result[0].yearBuilt,
+        lotSizeSqFt: results.response.results.result[0].lotSizeSqFt,
+    }
+    return home;
+}
 
 module.exports = router;
