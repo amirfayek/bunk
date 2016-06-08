cecb37ad29aa0fd59e440a2b31a5d0197ffb6228
Amir Fayek <afayek88@gmail.com>
2016-06-07 17:34:18 -0700

    update settings

--- models/db.js
+++ models/db.js
@@ -1,3 +1,9 @@
 var mongoose = require('mongoose');
+
 var uri = process.env.PROD_MONGODB || 'mongodb://localhost:27017/roommate-finder';
-mongoose.connect(uri);
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
+
+mongoose.connect(uri, options);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
