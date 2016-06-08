49b3df6c2e68299378a35880d2bcc589233f45df
Amir Fayek <afayek88@gmail.com>
2016-06-07 20:55:22 -0700

    add user reference to homes

--- models/homes.js
+++ models/homes.js
@@ -1,10 +1,12 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-var homeSchema = new mongoose.Schema({
-  address: String,
-  city: String,
-  state: String,
-  zpid: String
+var homeSchema = new Schema({
+    owner: { type : Schema.ObjectId, ref : 'User' },
+    address: String,
+    city: String,
+    state: String,
+    zpid: String
 });
 
 module.exports = mongoose.model('Home', homeSchema, 'home');
